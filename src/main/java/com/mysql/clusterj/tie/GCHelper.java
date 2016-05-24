35b498945a8df83848da13dc658f7457f122157d
Jim Dowling <jdowling@sics.se>
2016-05-24 20:43:51 +0200

    import

---
+++ src/main/java/com/mysql/clusterj/tie/GCHelper.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mysql.clusterj.tie;
+
+import java.nio.ByteBuffer;
+import sun.misc.Cleaner;
+import sun.misc.Unsafe;
+import sun.nio.ch.DirectBuffer;
+
+/**
+ *
+ * @author salman
+ */
+public class GCHelper {
+
+    public static void clean(ByteBuffer bb) {
+//        if (bb == null) {
+//            return;
+//        }
+//
+//        Cleaner cleaner = ((DirectBuffer) bb).cleaner();
+//        if (cleaner != null) {
+//            cleaner.clean();
+//        }
+    }
+}
