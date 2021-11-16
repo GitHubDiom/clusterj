83c30e4cfd9080914984a4549478067497cd056c
Scusemua <benrcarver@gmail.com>
2021-11-15 22:47:31 -0500

    Commented-out all events aside from the one on write_acks_deployment0

--- src/test/java/ClusterJHopsFSAckTableTest.java
+++ src/test/java/ClusterJHopsFSAckTableTest.java
@@ -1,7 +1,4 @@
-import com.mysql.clusterj.ClusterJHelper;
-import com.mysql.clusterj.Session;
-import com.mysql.clusterj.SessionFactory;
-import com.mysql.clusterj.TableEvent;
+import com.mysql.clusterj.*;
 import com.mysql.clusterj.core.store.EventOperation;
 import com.mysql.clusterj.tie.NdbEventOperationImpl;
 import com.mysql.ndbjtie.ndbapi.NdbRecAttr;
@@ -69,26 +66,30 @@ public class ClusterJHopsFSAckTableTest {
                 true);
 
         NdbEventOperationImpl eventOperation0 = (NdbEventOperationImpl)session.createEventOperation("ack_table_watch0");
-        NdbEventOperationImpl eventOperation1 = (NdbEventOperationImpl)session.createEventOperation("ack_table_watch1");
-        NdbEventOperationImpl eventOperation2 = (NdbEventOperationImpl)session.createEventOperation("ack_table_watch2");
-
-        NdbEventOperationImpl invEventOperation0 = (NdbEventOperationImpl)session.createEventOperation("inv_table_watch0");
-        NdbEventOperationImpl invEventOperation1 = (NdbEventOperationImpl)session.createEventOperation("inv_table_watch1");
-        NdbEventOperationImpl invEventOperation2 = (NdbEventOperationImpl)session.createEventOperation("inv_table_watch2");
+//        NdbEventOperationImpl eventOperation1 = (NdbEventOperationImpl)session.createEventOperation("ack_table_watch1");
+//        NdbEventOperationImpl eventOperation2 = (NdbEventOperationImpl)session.createEventOperation("ack_table_watch2");
+//
+//        NdbEventOperationImpl invEventOperation0 = (NdbEventOperationImpl)session.createEventOperation("inv_table_watch0");
+//        NdbEventOperationImpl invEventOperation1 = (NdbEventOperationImpl)session.createEventOperation("inv_table_watch1");
+//        NdbEventOperationImpl invEventOperation2 = (NdbEventOperationImpl)session.createEventOperation("inv_table_watch2");
 
         NdbRecAttr[] event0PreNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
         NdbRecAttr[] event0PostNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
-        NdbRecAttr[] event1PreNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
-        NdbRecAttr[] event1PostNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
-        NdbRecAttr[] event2PreNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
-        NdbRecAttr[] event2PostNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event1PreNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event1PostNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event2PreNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event2PostNdbRecAttributes = new NdbRecAttr[ACK_EVENT_COLUMNS.length];
+//
+//        NdbRecAttr[] event0PreNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event0PostNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event1PreNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event1PostNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event2PreNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
+//        NdbRecAttr[] event2PostNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
 
-        NdbRecAttr[] event0PreNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
-        NdbRecAttr[] event0PostNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
-        NdbRecAttr[] event1PreNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
-        NdbRecAttr[] event1PostNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
-        NdbRecAttr[] event2PreNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
-        NdbRecAttr[] event2PostNdbRecAttributesInv = new NdbRecAttr[INV_TABLE_EVENT_COLUMNS.length];
+        String dbugString = "d:t:L:F";
+        Dbug dbug = ClusterJHelper.newDbug();
+        dbug.push(dbugString);
 
         for (int i = 0; i < ACK_EVENT_COLUMNS.length; i++) {
             String eventColumnName = ACK_EVENT_COLUMNS[i];
@@ -97,44 +98,44 @@ public class ClusterJHopsFSAckTableTest {
             event0PostNdbRecAttributes[i] = postAttr0;
             event0PreNdbRecAttributes[i] = preAttr0;
 
-            NdbRecAttr postAttr1 = eventOperation1.getNdbEventOperation().getValue(eventColumnName, null);
-            NdbRecAttr preAttr1 = eventOperation1.getNdbEventOperation().getPreValue(eventColumnName, null);
-            event1PostNdbRecAttributes[i] = postAttr1;
-            event1PreNdbRecAttributes[i] = preAttr1;
-
-            NdbRecAttr postAttr2 = eventOperation2.getNdbEventOperation().getValue(eventColumnName, null);
-            NdbRecAttr preAttr2 = eventOperation2.getNdbEventOperation().getPreValue(eventColumnName, null);
-            event2PostNdbRecAttributes[i] = postAttr2;
-            event2PreNdbRecAttributes[i] = preAttr2;
+//            NdbRecAttr postAttr1 = eventOperation1.getNdbEventOperation().getValue(eventColumnName, null);
+//            NdbRecAttr preAttr1 = eventOperation1.getNdbEventOperation().getPreValue(eventColumnName, null);
+//            event1PostNdbRecAttributes[i] = postAttr1;
+//            event1PreNdbRecAttributes[i] = preAttr1;
+//
+//            NdbRecAttr postAttr2 = eventOperation2.getNdbEventOperation().getValue(eventColumnName, null);
+//            NdbRecAttr preAttr2 = eventOperation2.getNdbEventOperation().getPreValue(eventColumnName, null);
+//            event2PostNdbRecAttributes[i] = postAttr2;
+//            event2PreNdbRecAttributes[i] = preAttr2;
         }
 
-        for (int i = 0; i < INV_TABLE_EVENT_COLUMNS.length; i++) {
-            String eventColumnName = INV_TABLE_EVENT_COLUMNS[i];
-            NdbRecAttr postAttr0 = invEventOperation0.getNdbEventOperation().getValue(eventColumnName, null);
-            NdbRecAttr preAttr0 = invEventOperation0.getNdbEventOperation().getPreValue(eventColumnName, null);
-            event0PostNdbRecAttributesInv[i] = postAttr0;
-            event0PreNdbRecAttributesInv[i] = preAttr0;
-
-            NdbRecAttr postAttr1 = invEventOperation1.getNdbEventOperation().getValue(eventColumnName, null);
-            NdbRecAttr preAttr1 = invEventOperation1.getNdbEventOperation().getPreValue(eventColumnName, null);
-            event1PostNdbRecAttributesInv[i] = postAttr1;
-            event1PreNdbRecAttributesInv[i] = preAttr1;
-
-            NdbRecAttr postAttr2 = invEventOperation2.getNdbEventOperation().getValue(eventColumnName, null);
-            NdbRecAttr preAttr2 = invEventOperation2.getNdbEventOperation().getPreValue(eventColumnName, null);
-            event2PostNdbRecAttributesInv[i] = postAttr2;
-            event2PreNdbRecAttributesInv[i] = preAttr2;
-        }
+//        for (int i = 0; i < INV_TABLE_EVENT_COLUMNS.length; i++) {
+//            String eventColumnName = INV_TABLE_EVENT_COLUMNS[i];
+//            NdbRecAttr postAttr0 = invEventOperation0.getNdbEventOperation().getValue(eventColumnName, null);
+//            NdbRecAttr preAttr0 = invEventOperation0.getNdbEventOperation().getPreValue(eventColumnName, null);
+//            event0PostNdbRecAttributesInv[i] = postAttr0;
+//            event0PreNdbRecAttributesInv[i] = preAttr0;
+//
+//            NdbRecAttr postAttr1 = invEventOperation1.getNdbEventOperation().getValue(eventColumnName, null);
+//            NdbRecAttr preAttr1 = invEventOperation1.getNdbEventOperation().getPreValue(eventColumnName, null);
+//            event1PostNdbRecAttributesInv[i] = postAttr1;
+//            event1PreNdbRecAttributesInv[i] = preAttr1;
+//
+//            NdbRecAttr postAttr2 = invEventOperation2.getNdbEventOperation().getValue(eventColumnName, null);
+//            NdbRecAttr preAttr2 = invEventOperation2.getNdbEventOperation().getPreValue(eventColumnName, null);
+//            event2PostNdbRecAttributesInv[i] = postAttr2;
+//            event2PreNdbRecAttributesInv[i] = preAttr2;
+//        }
 
         System.out.println("Executing ACK event operations.");
         eventOperation0.execute();
-        eventOperation1.execute();
-        eventOperation2.execute();
+//        eventOperation1.execute();
+//        eventOperation2.execute();
 
-        System.out.println("Executing INV event operations.");
-        invEventOperation0.execute();
-        invEventOperation1.execute();
-        invEventOperation2.execute();
+//        System.out.println("Executing INV event operations.");
+//        invEventOperation0.execute();
+//        invEventOperation1.execute();
+//        invEventOperation2.execute();
 
         System.out.println("Listening for events now...");
         int eventCounter = 0;
@@ -166,41 +167,41 @@ public class ClusterJHopsFSAckTableTest {
                     preAttrs = event0PreNdbRecAttributes;
                     eventColumns = ACK_EVENT_COLUMNS;
                 }
-                else if (eventOperation1.equals(nextEventOp)) {
-                    now = Instant.now();
-                    System.out.println("[" + now.toString() + "] Received ACK Event for table write_acks_deployment1!");
-                    postAttrs = event1PostNdbRecAttributes;
-                    preAttrs = event1PreNdbRecAttributes;
-                    eventColumns = ACK_EVENT_COLUMNS;
-                }
-                else if (eventOperation2.equals(nextEventOp)) {
-                    now = Instant.now();
-                    System.out.println("[" + now.toString() + "] Received ACK Event for table write_acks_deployment2!");
-                    postAttrs = event2PostNdbRecAttributes;
-                    preAttrs = event2PreNdbRecAttributes;
-                    eventColumns = ACK_EVENT_COLUMNS;
-                }
-                else if (invEventOperation0.equals(nextEventOp)) {
-                    now = Instant.now();
-                    System.out.println("[" + now.toString() + "] Received INV Event for table inv_table_watch0!");
-                    postAttrs = event0PostNdbRecAttributesInv;
-                    preAttrs = event0PreNdbRecAttributesInv;
-                    eventColumns = INV_TABLE_EVENT_COLUMNS;
-                }
-                else if (invEventOperation1.equals(nextEventOp)) {
-                    now = Instant.now();
-                    System.out.println("[" + now.toString() + "] Received INV Event for table inv_table_watch1!");
-                    postAttrs = event1PostNdbRecAttributesInv;
-                    preAttrs = event1PreNdbRecAttributesInv;
-                    eventColumns = INV_TABLE_EVENT_COLUMNS;
-                }
-                else if (invEventOperation2.equals(nextEventOp)) {
-                    now = Instant.now();
-                    System.out.println("[" + now.toString() + "] Received INV Event for table inv_table_watch2!");
-                    postAttrs = event2PostNdbRecAttributesInv;
-                    preAttrs = event2PreNdbRecAttributesInv;
-                    eventColumns = INV_TABLE_EVENT_COLUMNS;
-                }
+//                else if (eventOperation1.equals(nextEventOp)) {
+//                    now = Instant.now();
+//                    System.out.println("[" + now.toString() + "] Received ACK Event for table write_acks_deployment1!");
+//                    postAttrs = event1PostNdbRecAttributes;
+//                    preAttrs = event1PreNdbRecAttributes;
+//                    eventColumns = ACK_EVENT_COLUMNS;
+//                }
+//                else if (eventOperation2.equals(nextEventOp)) {
+//                    now = Instant.now();
+//                    System.out.println("[" + now.toString() + "] Received ACK Event for table write_acks_deployment2!");
+//                    postAttrs = event2PostNdbRecAttributes;
+//                    preAttrs = event2PreNdbRecAttributes;
+//                    eventColumns = ACK_EVENT_COLUMNS;
+//                }
+//                else if (invEventOperation0.equals(nextEventOp)) {
+//                    now = Instant.now();
+//                    System.out.println("[" + now.toString() + "] Received INV Event for table inv_table_watch0!");
+//                    postAttrs = event0PostNdbRecAttributesInv;
+//                    preAttrs = event0PreNdbRecAttributesInv;
+//                    eventColumns = INV_TABLE_EVENT_COLUMNS;
+//                }
+//                else if (invEventOperation1.equals(nextEventOp)) {
+//                    now = Instant.now();
+//                    System.out.println("[" + now.toString() + "] Received INV Event for table inv_table_watch1!");
+//                    postAttrs = event1PostNdbRecAttributesInv;
+//                    preAttrs = event1PreNdbRecAttributesInv;
+//                    eventColumns = INV_TABLE_EVENT_COLUMNS;
+//                }
+//                else if (invEventOperation2.equals(nextEventOp)) {
+//                    now = Instant.now();
+//                    System.out.println("[" + now.toString() + "] Received INV Event for table inv_table_watch2!");
+//                    postAttrs = event2PostNdbRecAttributesInv;
+//                    preAttrs = event2PreNdbRecAttributesInv;
+//                    eventColumns = INV_TABLE_EVENT_COLUMNS;
+//                }
                 else {
                     throw new IllegalStateException("Unknown event operation: " + nextEventOp);
                 }
